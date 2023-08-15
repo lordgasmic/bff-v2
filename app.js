8ae4fdc02842fb75e6119c0e8195749713b21b67
Nick Wellman <wellman.nicholas@gmail.com>
2023-08-14 22:16:48 -0400

    set up login route

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
 
-var indexRouter = require("./routes");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes");
+const usersRouter = require("./routes/users");
+const loginRouter = require("./routes/login");
 
-var app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/api/v1/login", loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
