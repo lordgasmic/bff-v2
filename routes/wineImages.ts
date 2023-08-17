a277539ab2a7781feee15431f8d07eb67cd311ef
Nick Wellman <wellman.nicholas@gmail.com>
2023-08-17 11:33:46 -0400

    disable put images

--- routes/wineImages.ts
+++ routes/wineImages.ts
@@ -21,33 +21,33 @@ wineImagesRouter.get("/", async function (req, res, next) {
   return res.send(response);
 });
 
-wineImagesRouter.put("/", async function (req, res, next) {
-  /*
-         @RequestParam("wineId") final int wineId,
-         @RequestParam("label") final String label,
-         @RequestParam("imageFile") final MultipartFile imageFile,
-         @RequestHeader final Map<String, String> headers
-       */
-
-  const response = await fetch(
-    `${services["collection-service"]}/api/v1/wineImages`,
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        wineryId: req.body.wineryId,
-        name: req.body.name,
-        style: req.body.style,
-      }),
-    },
-  )
-    .then((res) => res.json())
-    .then((json) => {
-      return json;
-    });
-  return res.send(response);
-});
+// wineImagesRouter.put("/", async function (req, res, next) {
+//   /*
+//          @RequestParam("wineId") final int wineId,
+//          @RequestParam("label") final String label,
+//          @RequestParam("imageFile") final MultipartFile imageFile,
+//          @RequestHeader final Map<String, String> headers
+//        */
+//
+//   const response = await fetch(
+//     `${services["collection-service"]}/api/v1/wineImages`,
+//     {
+//       method: "PUT",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         wineryId: req.body.wineryId,
+//         name: req.body.name,
+//         style: req.body.style,
+//       }),
+//     },
+//   )
+//     .then((res) => res.json())
+//     .then((json) => {
+//       return json;
+//     });
+//   return res.send(response);
+// });
 
 export default wineImagesRouter;
